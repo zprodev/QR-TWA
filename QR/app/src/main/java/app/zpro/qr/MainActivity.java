3944385ce235c405788a0879ef23b13e23dfce3c
zprodev <zproject.develop@gmail.com>
2019-02-22 17:05:57 +0900

    Set Toolbar color

--- QR/app/src/main/java/app/zpro/qr/MainActivity.java
+++ QR/app/src/main/java/app/zpro/qr/MainActivity.java
@@ -1,5 +1,6 @@
 package app.zpro.qr;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.customtabs.CustomTabsSession;
@@ -13,7 +14,10 @@ public class MainActivity extends android.support.customtabs.trusted.LauncherAct
 
     @Override
     protected CustomTabsIntent getCustomTabsIntent(CustomTabsSession session) {
+        Resources res = getResources();
+        int primaryDarkColor = res.getColor(R.color.colorPrimaryDark);
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(session);
+        builder.setToolbarColor(primaryDarkColor);
         return builder.build();
     }
 }
